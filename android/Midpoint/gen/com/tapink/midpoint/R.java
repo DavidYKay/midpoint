fddaa835f62821af19dfb6e220a279de3183c5e0
David Young-Chan Kay <davidykay@gmail.com>
2011-11-05 14:17:14 -0400

    Added new resources: buttons, etc

--- android/Midpoint/gen/com/tapink/midpoint/R.java
+++ android/Midpoint/gen/com/tapink/midpoint/R.java
@@ -8,20 +8,33 @@
 package com.tapink.midpoint;
 
 public final class R {
+    public static final class array {
+        public static final int map_actions=0x7f050000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int button_cancel=0x7f060001;
+        public static final int red=0x7f060000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_black=0x7f020000;
+        public static final int btn_blue=0x7f020001;
+        public static final int btn_green=0x7f020002;
+        public static final int btn_purple=0x7f020003;
+        public static final int btn_red=0x7f020004;
+        public static final int btn_yellow=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int button=0x7f050007;
-        public static final int button_cancel=0x7f050003;
-        public static final int button_confirm=0x7f050002;
-        public static final int footer_confirm=0x7f050001;
-        public static final int list=0x7f050000;
-        public static final int mapview=0x7f050006;
-        public static final int my_location=0x7f050004;
-        public static final int their_location=0x7f050005;
+        public static final int button=0x7f080007;
+        public static final int button_cancel=0x7f080003;
+        public static final int button_confirm=0x7f080002;
+        public static final int footer_confirm=0x7f080001;
+        public static final int list=0x7f080000;
+        public static final int mapview=0x7f080006;
+        public static final int my_location=0x7f080004;
+        public static final int their_location=0x7f080005;
     }
     public static final class layout {
         public static final int confirm_venue=0x7f030000;
@@ -30,19 +43,23 @@ public final class R {
         public static final int pick_venue=0x7f030003;
     }
     public static final class string {
-        public static final int advance=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int calendar=0x7f040005;
-        public static final int cancel=0x7f040003;
-        public static final int confirm=0x7f040004;
-        public static final int confirm_venue=0x7f040008;
-        public static final int hello=0x7f040000;
-        public static final int location=0x7f040006;
-        public static final int maps_api_key=0x7f04000c;
-        public static final int maps_dev_key=0x7f04000d;
-        public static final int my_location=0x7f04000a;
-        public static final int pick_venue=0x7f040007;
-        public static final int push_me=0x7f040009;
-        public static final int their_location=0x7f04000b;
+        public static final int advance=0x7f040005;
+        public static final int app_name=0x7f040004;
+        public static final int calendar=0x7f040008;
+        public static final int cancel=0x7f040006;
+        public static final int confirm=0x7f040007;
+        public static final int confirm_venue=0x7f04000b;
+        public static final int dk_dev_key=0x7f040001;
+        public static final int hello=0x7f040003;
+        public static final int kennedy_dev_key=0x7f040002;
+        public static final int location=0x7f040009;
+        public static final int maps_api_key=0x7f040000;
+        public static final int my_location=0x7f04000d;
+        public static final int pick_venue=0x7f04000a;
+        public static final int push_me=0x7f04000c;
+        public static final int their_location=0x7f04000e;
+    }
+    public static final class style {
+        public static final int ButtonText=0x7f070000;
     }
 }
